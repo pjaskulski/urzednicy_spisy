069cbbb4fee269d70c7b02e55e95eccaada084c8
Piotr Jaskulski <piotr.jaskulski@gmail.com>
2022-05-25 23:06:22 +0200

    pomorze

---
+++ src/urzednicy_pomorza_literatura.py
@@ -0,0 +1,112 @@
+literatura = {
+
+"BienKaszt.":"J. Bieniak, Przynależność administracyjno polityczna kasztelanii  nadgoplańskich w latach 1267- 1327, “Zeszyty Naukowe UMK w Toruniu. Historia”, 2, 1966", 
+
+"BienWlkp.": "J. Bieniak, Wielkopolska, Kujawy, ziemie łęczycka i sieradzka wobec problemu zjednoczenia państwowego w l. 1300- 1306, Toruń 1969", 
+
+"Bobowski": "K. Bobowski, Ze studiów nad autentycznościa wczesnośredniowiecznych dokumentów zachodniopomorskich, “Acta Universitatis Wratislaviensis. Historia”, 33, 1980",
+
+"Bruski": "K. Bruski, Sprawa autentyczności dokumentów Sambora II dla cystersów oliwskich, “Zeszyty Naukowe Wydziału Humanistycznego Uniwersytetu Gdańskiego. Historia”, t. XV, 1985 (druk 1987), s. 5 i n.",
+
+"Chron.": "Chronologia polska, pod red. B. Włodarskiego, Warszawa 1957.",
+
+"CzachŻuk.": "A. Czacharowski, Uposażenie i organizacja klasztoru norbertanek w Żukowie, Toruń 1963",
+
+"DKM": "Dokumenty kujawskie i mazowieckie przeważnie z XIII wieku, wyd. B. Ulanowski, Kraków 1888",
+
+"GórItin.": "Z. Górski, Itinerarium Mszczuja II, “Zeszyty Naukowe UMK, w Toruniu. Historia”, 8, 1973", 
+
+"GórOznacz.": "Z. Górski, Oznaczenie dnia w dokumentach książąt Pomorza Gdańskiego, “Prace Komisji Historycznej Bydgoskiego Towarzystwa Naukowego”, 11, 1975",
+ 
+"JasChron.": "K. Jasiński, Chronologia kościołów gdańskich XII i XIII wieku “Zapiski Historyczne”, 50, 1985, z. 1",
+
+"JasKilka": "K. Jasiński, Kilka uwag o najstarszych dokumentach Pomorza Gdańskiego, “Studia Źródłoznawcze”, 2, 1958", 
+
+"JasUw.": "K. Jasiński, Uwagi nad kancelarią Władysława Łokietka Kazimierza Wielkiego, “Zapiski Historyczne”, 19, 1953, z. 1-4",
+
+"JasWysz.": "K. Jasiński, Wyszelice. Z dziejów możnowładztwa pomorskiego na przełomie XIII i XIV w., “Zapiski Historyczne”, 22, 1956, z. 1-3",
+
+"KDW": "Kodeks dyplomatyczny Wielkopolski, t. I- III, Poznań 1877- 1879, t. VI, Warszawa 1982",
+
+"Koebner": "R. Koebner, Urkundenstudien zur Geschichte Danzigs und Olivas von 1178 bis 1342, “Zeitschrift des Westpreussischen  Geschichtsvereins” 71, 1934", 
+
+"Kozłowski": "R. Kozłowski, Rozwój uposażenia klasztoru cysterskiego w By­szewie (Koronowie), Warszawa 1972",
+
+"KPol.": "Kodeks dyplomatyczny Polski, wyd. L. Rzyszczewski, A. Muczkowski , A. Z. Helcel, J. Bartoszewicz, M. Bobowski, t. I- IV, Warszawa 1847­-1887",
+
+"LabBad.": "G. Labuda, Z badań nad genealogią książąt Pomorza Gdańskiego, w XII i XIII wieku, “Rocznik Gdański”, 41, 1981, nr 1",
+
+"LabGen.": "G. Labuda, Geneza miasta na “prawie lubeckim” w Gdańsku, “Kwartalnik Historii Kultury Materialnej”, 29, 1981, nr 1",
+
+"LabPoch.": "G. Labuda, O pochodzeniu i stanowisku polityczno-prawnym dynastii wschodniopomorskich, szczególnie sławieńskiej, [w:] tegoż, Fragmenty dziejów Słowiańszczyzny zachodniej, t. III, Poznań 1975",
+
+"LabSt.": "G. Labuda, Ze studiów nad najstarszymi dokumentami Pomorza Gdańskiego, “Zapiski Historyczne”, 18, 1953, z. 1 - 4",
+
+"LabŹród.": "G. Labuda, O źródłach Kroniki pruskiej Piotra z Dusburga, (95 “Komunikaty Mazursko-Warmińskie”, nr 2-3, 1971",
+
+"Ling.": "H. Lingenberg, Die Anfänge des Klosters Oliva und die Entstehung der deutschen Stadt Danzig, Stuttgart 1882",
+
+"Lites 1321": "Lites ac res gestae inter Polonos Ordinemque Cruciferorum, wyd. H. Chłopocka, Wrocław 1970",
+
+"Lites 1339": "Lites ac res gestae... [wyd. I. Zakrzewski], Poznań 1890", 
+
+"Ludzie": "Ludzie pomorskiego średniowiecza. Szkice biograficzne, Gdańsk 1981", 
+
+"Morré": "P. Morré, Die Swenzonen in Ostpommern. Aufstieg und Herrschaft 1269-1357, “Baltische Studien”, Neue Fo1ge, 41, 1939",
+
+"MPH": "Monumenta Poloniae historica, t. I- VI, Lwów- Kraków 1871 - 1893, nowa seria (s.n.), t. VI, Warszawa 1962",
+
+"P": "Pommerellisches Urkundenbuch, wyd. M. Perlbach, Danzig 1882", 
+
+"PopFał.": "B. Popielas-Szultka, Fałszerstwa, interpolacje czy nadania autentycz­ne dokumentów opactwa bukowskiego dotyczące Malechowa, [w:] Z badań nad społeczeństwem i kulturą Pomorza Środkowego, Słupsk 1977", 
+
+"PopRozw.": "B. Popielas-Szultka, Rozwój gospodarczy dominium bukowskiego    od połowy XIII do połowy XVI w., Słupsk 1980",
+
+"PowPrzycz.": "J. Powierski, Przyczynek do dziejów politycznych Pomorza Gdańskiego na przełomie XIII i XIV w., “Zeszyty Naukowe UMK  w Toruniu. Historia”, 2, 1966",
+
+"PowUkład": "J. Powierski, Układ kamieński (1264) na tle stosunków między książętami pomorskimi, Krzyżakami i Prusami w latach 60-tych XIII w., “Rocznik Olsztyński”, 8, 1968",
+
+"PowUwagi": "J. Powierski, Uwagi o wydarzeniach kujawskich w latach 1267- 1271, “Prace Komisji Historycznej Bydgoskiego Towarzystwa Naukowego”, 5, 1968",
+
+"PrUB": "Preussisches Urkundenbuch, wyd. R. Philippi, A. Seraphim, M. Hein, H. Koeppen, Königsberg 1882-1944, Marburg 1958 i n.", 
+
+"PSB": "Polski Słownik Biograficzny, Kraków 1935-1986", 
+
+"PU": "Pommersches Urkundenbuch, wyd. R. Klempin, R. Prümers, O. Heinemann, H. Frederichs, E. Sandow, Stettin 1868-1906",
+
+"Rymar": "E. Rymar, Problem autentyczności dokumentu księcia gdańskiego Świętopełka data 20 stycznia 1205 r. i jego przydatność do badań nad dziejami Pomorza Sławieńsko-Słupskiego w pierwszej połowie XIII wieku, “Rocznik Gdański”, 40, 1980, z. 1, s. 39-64", 
+
+"SpDz.": "J. Spors, Dzieje polityczne ziem sławieńskiej, słupskiej i białogardzkiej XII- XIV w., Poznań 1973",
+
+"SpPodz.": "J. Spors, Podziały administracyjne Pomorza Gdańskiego i Słowieńsko-Słupskiego od XII do początku XIV w., Słupsk 1983",
+
+"SpProb.": "J. Spors, Problem autentyczności dokumentu rzekomo z 1205 roku “Rocznik Słupski”, 1980",
+
+"SpRola": "J. Spors, Rola polityczna Święców w końcu XIII i początku XIV w., “Roczniki Historyczne”, 46, 1980", 
+
+"SpRoz.": "J. Spors, Dzieje pomorskiego rodu rycerskiego Rozwarowiców w XII i na początku XIV w., “Słupskie Prace Humanistyczne”, 5, 1985", 
+
+"SpU.": "J. Spors, Urzędnicy terytorialni i nadworni na Pomorzu Gdańskim i Sławieńsko-Słupskim w XIII i początku XIV w., “Czasopismo Prawno-Historyczne”, 33, 1981, z. 2",
+
+"SpWoj.": "J. Spors, Urząd wojewody na Pomorzu Gdańskim do początków XIV w., “Słupskie Prace Humanistyczne”, 1985",
+
+"SRP": "Scriptores rerum Prussicarum. Die Geschichtsguellen der preussischen Vorzeit bis zum Untergange der Ordensherrschaft, wyd. T. Hirsch, M. Toeppen, E. Strehlke, t. I-V, Leipzig 1861- 1874",
+
+"ŚliwDow.": "B. Śliwiński, Dowódcy grodu gdańskiego w 1308 roku, “Zapiski Historyczne”, 53, 1988, z. 1/2",
+
+"ŚliwPrzyw.": "B. Śliwiński, Pierwszy przywilej rycerski księcia Mściwoja II. Krytyka autentyczności dokumentu dla Ścibora z Gacek z 1226 roku, “Studia Źródłoznawcze” (w druku)",
+
+"ŚliwRola": "B. Śliwiński, Rola polityczna możnowładztwa na Pomorzu Gdańskim w czasach Mściwoja II, Gdańsk 1987, s. 182-183",
+
+"ŚliwRyc.": "B. Śliwiński, Rola polityczna rycerstwa gdańskiego w okresie wojny domowej na Pomorzu Gdańskim w latach 1269-1272, “Zapiski Historyczne”, 51, 1986, z. 2",
+
+"Tęgowski": "J. Tęgowski, O włodarzach w Polsce XIII wieku, “Studia i Materiały do Dziejów Wielkopolski i Pomorza”, 15, 1983, z. 1",
+
+"Wenskus": "R. Wenskus, Eine prussische Familie in Pommerellen und ihre Erben, [w:] Europa Slavica - Europa Orientalis, Berlin 1980",
+
+"Wernicke": "E. Wernicke, Drei Urkunden über das Dorf Adl. Gremblin, “Zeitschrift des historischen Vereins für de	n Regierungsbezirk Marienwerder”, 51, 1901",
+
+"ZielPrzyw.": "K. Zielińska, Przywilej Mszczuja II z 1277 roku dla kasztelana puckiego Czcibora, “Zeszyty Naukowe UMK w Toruniu. Historia”, 8, 1973",
+
+"ZielZjed.": "K. Zielińska, Zjednoczenie Pomorza Gdańskiego z Wielkopolska pod koniec XIII w. Umowa kępińska 1282 roku, Toruń 1968"
+}
